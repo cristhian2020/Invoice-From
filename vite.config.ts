955812532cfc64a9360e5f7e4aa05aa6110327d3
CristhianRF24 <201803525@est.umss.edu>
2026-07-22 10:40:56 -0400

    se quito la seccion de SENT

--- vite.config.ts
+++ vite.config.ts
@@ -18,16 +18,21 @@ export default defineConfig({
         short_name: 'Hours Report',
         description: 'Hours Report Application',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
         icons: [
           {
             src: 'logo.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'logo.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
